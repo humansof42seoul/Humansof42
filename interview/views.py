38759b53c1d03390acf20a30870cf74d351e155b
yechoi42 <yechoi@student.42seoul.kr>
2021-03-14 21:40:11 +0900

    [FIX] changed permission from admin to staff

--- interview/views.py
+++ interview/views.py
@@ -61,7 +61,7 @@ def interview_detail(request, pk):
 
 
 @login_required
-@permission_required('user.is_admin')
+@permission_required('user.is_staff')
 def interview_write(request):
     # if not request.user.is_admin:
     #     return redirect('/interviews/')
@@ -84,7 +84,7 @@ def interview_write(request):
 
 
 @login_required
-@permission_required('user.is_admin')
+@permission_required('user.is_staff')
 def interview_edit(request, pk):
     try:
         interview = Interview.objects.get(pk=pk)
