8becc22d3254d71a20b5cc17baf983944a39f47e
yechoi42 <yechoi@student.42seoul.kr>
2022-01-18 23:05:19 +0900

    feat: add email-password form

--- user/views.py
+++ user/views.py
@@ -38,34 +38,26 @@ def change_password(request):
     return render(request, 'user/mypage.html', {'email_form': email_form, 'password_form': form})
 
 def log_in(request):
+    form = LoginForm()
+    ft_state = get_random_string(42)
+    request.session['ft_state'] = ft_state
+    ft_api_sign_in = "https://api.intra.42.fr/oauth/authorize"
+    redirect_uri = f"https://{request.get_host()}{reverse('ft_login')}"
+    response_type = "code"
+    scope = "public"
+    ft_sign_in_url = f"{ft_api_sign_in}?client_id={settings.FT_UID_KEY}&redirect_uri={redirect_uri}&response_type={response_type}&state={ft_state}&scope={scope}"
     if request.method == 'GET':
-        ft_state = get_random_string(42)
-        request.session['ft_state'] = ft_state
-        ft_api_sign_in = "https://api.intra.42.fr/oauth/authorize"
-        redirect_uri = f"https://{request.get_host()}{reverse('ft_login')}"
-        response_type = "code"
-        scope = "public"
-        ft_sign_in_url = f"{ft_api_sign_in}?client_id={settings.FT_UID_KEY}&redirect_uri={redirect_uri}&response_type={response_type}&state={ft_state}&scope={scope}"
         return render(request, 'user/sign_in.html', {'ft_sign_in_url': ft_sign_in_url})
-    # form = LoginForm()
-    # ft_state = get_random_string(42)
-    # request.session['ft_state'] = ft_state
-    # ft_api_sign_in = "https://api.intra.42.fr/oauth/authorize"
-    # redirect_uri = f"https://{request.get_host()}{reverse('ft_login')}"
-    # # redirect_uri = f"http://{request.get_host()}{reverse('ft_login')}"
-    # response_type = "code"
-    # scope = "public"
-    # ft_sign_in_url = f"{ft_api_sign_in}?client_id={settings.FT_UID_KEY}&redirect_uri={redirect_uri}&response_type={response_type}&state={ft_state}&scope={scope}"
-    # if request.method == 'POST':
-    #     form = LoginForm(request.POST, request.user)
-    #     email = request.POST['email']
-    #     password = request.POST['password']
-    #     user = find_user_with_email(email)
-    #     user = authenticate(login = user.login, password = password)
-    #     if user:
-    #         login(request, user)
-    #         return redirect('main')
-    # return render(request, 'user/sign_in.html', {'ft_sign_in_url': ft_sign_in_url, 'form': form})
+    elif request.method == 'POST':
+        form = LoginForm(request.POST, request.user)
+        email = request.POST['email']
+        password = request.POST['password']
+        user = find_user_with_email(email)
+        user = authenticate(login = user.login, password = password)
+        if user:
+            login(request, user)
+            return redirect('main')
+    return render(request, 'user/sign_in.html', {'ft_sign_in_url': ft_sign_in_url, 'form': form})
 
 
 def ft_log_in(request):
