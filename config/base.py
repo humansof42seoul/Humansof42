8c3ff71760f954913048a9802bee7a268ebe9d52
yechoi42 <yechoi@student.42seoul.kr>
2022-01-27 23:01:41 +0900

    chore: split protocol with secret_into_file.json

--- config/base.py
+++ config/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os, json
-from pathlib import Path
 from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
