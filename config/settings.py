b0ace7ed642ca76567e6972ff10a145a83c68358
yechoi42 <yechoi@student.42seoul.kr>
2022-06-29 14:43:58 +0000

    chore: finished migrationgit add .git add .git add .!

--- config/settings.py
+++ config/settings.py
@@ -205,7 +205,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
 	STATIC_DIR,
 ]
-STATIC_ROOT = os.path.join(ROOT_DIR, '.static_root')
+STATIC_ROOT = os.path.join(ROOT_DIR, '/Humansof42/.static_root')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
