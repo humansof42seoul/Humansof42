d74163a604c06253e3717ebc506e711b76fc9acc
yechoi42 <yechoi@student.42seoul.kr>
2022-07-12 14:51:49 +0000

    feat: add set_og_info.js

---
+++ static/set_og_info.js
@@ -0,0 +1,4 @@
+document.title = document.getElementById('title').innerText;
+let og_image_tag = document.querySelector("meta[property='og:image']");
+og_image_tag.setAttribute("content", "marie_gonzu");
+        
