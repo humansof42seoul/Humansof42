3c008fa1e922fc6dd08a685cd842625d7d5de2a6
yechoi42 <yechoi@student.42seoul.kr>
2022-07-14 12:02:53 +0000

    feat: add og_title setter  on set_og_info.js

--- static/set_og_info.js
+++ static/set_og_info.js
@@ -1,4 +1,6 @@
-document.title = document.getElementById('title').innerText;
+let title = document.getElementById('title').innerText;
+document.title = title;
 let og_image_tag = document.querySelector("meta[property='og:image']");
-og_image_tag.setAttribute("content", "marie_gonzu");
-        
+og_image_tag.setAttribute("content", "marie_gonzu");        
+let og_title_tag = document.querySelector("meta[property='og:title']");
+og_title_tag.setAttribute("content", "Humans of 42 | " + title);
