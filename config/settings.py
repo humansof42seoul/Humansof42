c1ca9b12b9e8d038c1d983b252bd4394d931d41b
yechoi42 <yechoi@student.42seoul.kr>
2022-07-20 14:04:07 +0000

    fix: set og info from server-side

--- config/settings.py
+++ config/settings.py
@@ -156,7 +156,6 @@ TEMPLATES = [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
